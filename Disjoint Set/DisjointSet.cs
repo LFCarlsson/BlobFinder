be73e4422ae946372f9eac3fa7832e135b09cc6a
Fredrik Carlsson <frecar89@gmail.com>
2017-12-08 16:00:52 +0100

    Added the disjoint set library into the project directory.

---
+++ Disjoint Set/DisjointSet.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disjoint_Set
+{
+    public class DisjointSet<T> where T : IComparable<T>
+    {
+
+        private List<DisjointSetNode<T>> sets;
+
+        public List<DisjointSetNode<T>> Sets { get => sets; private set => sets = value; }
+
+        public DisjointSet()
+        {
+            Sets = new List<DisjointSetNode<T>>();
+        }
+
+        override public string ToString()
+        {
+            string result = "";
+            for(int i = 0; i < sets.Count(); i++)
+            {
+                result += sets[i];
+                if(i != sets.Count -1)
+                    result += "\n";
+            }
+            return result;
+            
+        }
+
+        public void AddSet(T value)
+        {
+            AddSet(new T[] { value });
+        }
+
+        public int Count()
+        {
+            return Sets.Count();
+        }
+
+        public void AddSet(T[] values)
+        {
+            var newSet = new SortedSet<T>();
+            foreach(T value in values)
+            {
+                newSet.Add(value);
+            }
+            Sets.Add(new DisjointSetNode<T>(newSet));
+        }
+
+        public DisjointSetNode<T> Find(T value)
+        {
+            foreach(var set in Sets)
+            {
+                if (set.Contains(value)) {
+                    return set;
+                }
+            }
+            throw (new KeyNotFoundException("value not in set"));
+        }
+
+        public T FindRepresentant (T value)
+        {
+            return Find(value).Representant;
+        }
+
+        public void Merge(T firstVal, T secondVal)
+        {
+            DisjointSetNode<T> firstSet = Find(firstVal);
+            DisjointSetNode<T> secondSet = Find(secondVal);
+            int firstCompSecond = firstSet.Representant.CompareTo(secondSet.Representant);
+            if (firstCompSecond < 0)
+            {
+                firstSet.Set.UnionWith(secondSet.Set);
+                sets.Remove(secondSet);
+            }
+            if (firstCompSecond > 0)
+            {
+                secondSet.Set.UnionWith(firstSet.Set);
+                sets.Remove(firstSet);
+            }
+        }
+
+        private DisjointSetNode<T> GetSetFromRepresentant(T representant)
+        {
+            foreach(var set in Sets)
+            {
+                if (set.Representant.Equals(representant))
+                {
+                    return set;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ Disjoint Set/DisjointSetNode.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Disjoint_Set
+{
+    public class DisjointSetNode<T> where T : IComparable<T>
+    {
+        T representant; // the smallest value in the set.
+        SortedSet<T> set;
+        public SortedSet<T> Set { get => set; private set => this.set = value; }
+        public T Representant { get => representant; internal set => representant = value; }
+
+        public DisjointSetNode(T value)
+        {
+            Set = new SortedSet<T>();
+            Set.Add(value);
+            Representant = value;
+        }
+        /// <summary>
+        /// Check if the set contains a certain value
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool Contains(T value)
+        {
+            return set.Contains(value);
+        }
+        public DisjointSetNode(SortedSet<T> set)
+        {
+            Set = set;
+            Representant = set.Min;
+        }
+
+        public override string ToString()
+        {
+            string result = string.Join(",", set);
+            result = result.Insert(0, "{");
+            result += "}";
+            return result;
+        }
+    }
+}
